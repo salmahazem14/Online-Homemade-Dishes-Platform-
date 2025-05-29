49077964ca7521034d1fd7c626d714db68117fac
salmahazem14 <150745682+salmahazem14@users.noreply.github.com>
2025-05-29 16:20:27 +0300

    Delete repository directory

--- repository/DummyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.dummy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DummyRepository extends JpaRepository<dummy, Long> {
-}
